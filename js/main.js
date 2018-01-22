47a9e8bd3c0f8040ed3e93746c02a9e0ca12635d
Kumar Gaurav <grv.rkg@gmail.com>
2018-01-23 03:19:05 +0530

    subjected the theme

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
+  
+  // slider conf and init
   $('.slider').slider({
     'indicators':true,
     'height':500,
   });
+
+  //entore card as a tag 
+  // $(".card").click(function() {
+  //   window.location = $(this).find("a").attr("href"); 
+  //   return false;
+  // });
+  
+  //overlay for better visibility of random texts on random colorful images
+  $.each( $(".blur-title-overlay"), function( index, value ){
+    
+    //get the dynamic values
+    var textWidth = $(value).next().css('width');
+    var imageURL = $(value).prev().attr('src');
+    
+    //set the dynamic values
+    $(value).css('width',textWidth);
+    $(value).css('background-image','url("' + imageURL + '") ');
+
+  });
+
+  
 });
